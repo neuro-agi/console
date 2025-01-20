5bd3aeaabb4b00c4267703274efda03dd57ae976
khushmanvar <khushmanvar@gmail.com>
2025-01-20 10:15:22 +0530

    build: disable minification in prod build
    - Disable JS minification
    - Disable chunk splitting
    - Disable runtime chunk

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: false,
-  productionBrowserSourceMaps: true
+  productionBrowserSourceMaps: true,
+  webpack: (config, { dev }) => {
+    if (!dev) {
+      config.optimization.minimize = false;
+      config.optimization.splitChunks = false;
+      config.optimization.runtimeChunk = false;
+    }
+    return config;
+  }
 };
 
 export default nextConfig;
